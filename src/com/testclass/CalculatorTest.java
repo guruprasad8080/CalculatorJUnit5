396d6b88794a2b01e0e11a6c5752b96ca42c350a
Guruprasad <guru8080.gp@gmail.com>
2025-02-24 11:24:53 +0530

    Update CalculatorTest.java

--- src/com/testclass/CalculatorTest.java
+++ src/com/testclass/CalculatorTest.java
@@ -1,5 +1,9 @@
 package com.testclass;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,58 +11,68 @@ import org.junit.jupiter.api.Test;
 import com.junitclass.Calculator;
 
 public class CalculatorTest {
+
+	// Test the Addition method
+	// <methodNameToBeTested><Valid><Test>
+	/*
+	 * public int addition(int num1,int num2) { return num1+num2; }
+	 */
+
+	Calculator c;
+
+	@BeforeEach
+	public void beforEachMethod() {
+		System.out.println("Initializating the calculator instance ");
+		c = new Calculator();
+	}
 	
-	//Test the Addition method
-	//<methodNameToBeTested><Valid><Test>
-             /*	public int addition(int num1,int num2) {
-	             	return num1+num2;
-	            }
-             */
+	@AfterEach
+	public void afterEachMethod() {
+		System.out.println("This method is excuted After Each the method ");
+	}
+
+	@BeforeAll
+	public static void beforAllMethod() {
+		System.out.println("This method is excuted Before all the method ");
+	}
 	
-	
-	//testMethod
+	@AfterAll
+	public static void afterAllMethod() {
+		System.out.println("This method is excuted  After All  the method ");
+	}
+
+	// testMethod
 	@Test
 	@DisplayName("Adding two Integer Number")
 	void additionValidTest() {
-		
-		Calculator c=new Calculator();
-		
-		//actual value == expected value
-		int expectedvalue=5;
-		int actualValue=c.addition(2,3);
-		
-		System.out.println("The Sum of the method: "+actualValue);
+
+		// actual value == expected value
+		int expectedvalue = 5;
+		int actualValue = c.addition(2, 3);
+
+		System.out.println("The Sum of the method: " + actualValue);
 	}
-	
-	
+
 	@Test
 	@DisplayName("Adding two Negative Integer Number")
 	void additionValidTest1() {
-		
-		Calculator c=new Calculator();
-		
-		//actual value == expected value
-		int expectedvalue=5;
-		int actualValue=c.addition(-2,-3);
-		
-		System.out.println("The Sum of the method: "+actualValue);
+
+		// actual value == expected value
+		int expectedvalue = 5;
+		int actualValue = c.addition(-2, -3);
+
+		System.out.println("The Sum of the method: " + actualValue);
 	}
-	
+
 	@Disabled
 	@Test
 	void additionValidTest2() {
-		
-		Calculator c=new Calculator();
-		
-		//actual value == expected value
-		int expectedvalue=5;
-		int actualValue=c.addition(2,3);
-		
-		System.out.println("The Sum of the method: "+actualValue);
+
+		// actual value == expected value
+		int expectedvalue = 5;
+		int actualValue = c.addition(2, 3);
+
+		System.out.println("The Sum of the method: " + actualValue);
 	}
-	
-	
-	
-	
 
 }
